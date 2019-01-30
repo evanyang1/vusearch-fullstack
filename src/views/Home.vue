ea1560f1ecd7fe58c46d8549b26dfcfdfe4c5162
Evan Yang <evanjyang4@gmail.com>
2019-01-30 01:30:44 -0800

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <h1><a href="/">vuSearch</a></h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Quicksand');
+h1{
+  font-family: 'Quicksand' , sans-serif;
+}
+
+</style>
